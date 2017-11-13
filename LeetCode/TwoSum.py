eb28ce279d4cb0d904c86eaa222bffb94adc09a9
DuYeHong <c.fans@foxmail.com>
2017-11-14 00:05:36 +0800

    Complete TwoSum

--- LeetCode/TwoSum.py
+++ LeetCode/TwoSum.py
@@ -9,17 +9,9 @@ class Solution:
             raise ValueError("nums must have two or greater elements.")
         result = []
         for frist_index, frist in enumerate(nums):
-            print(frist_index)
-            print(frist)
-            for seconde_index,seconde in enumerate(nums[frist_index+1:]):
+            distance = frist_index + 1
+            for seconde_index,seconde in enumerate(nums[distance:]):
                 if frist + seconde == target:
-                    result.append([frist_index, seconde_index])
-                    return result
+                    return [frist_index, seconde_index + distance]
 
         return []  # return empty list  if not found the solution
-
-t = [3, 2, 4]
-target = 6
-s = Solution()
-print(t[0:])
-print(s.twoSum(t, target))
