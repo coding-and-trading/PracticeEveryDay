f78bdcec16cd8712aab9f6803bfa2e6abccf908b
DuYeHong <c.fans@foxmail.com>
2018-02-26 23:44:13 +0800

    update

--- DSAndAlgorithmsInPython/chapter3/LList.py
+++ DSAndAlgorithmsInPython/chapter3/LList.py
@@ -13,9 +13,29 @@ class LList:
     def prepend(self, elem):
         self._head = LNode(elem, self._head)
 
+    def append(self, elem):
+        if self._head is None:
+            self._head = LNode(elem)
+            return
+        p = self._head
+        while p.next is not None:
+            p = p.next
+        p.next = LNode(elem)
+
     def pop(self):
         if self._head is None:
             raise LinkedListUnderflow('in pop')
         e = self._head.elem
-        self._head = self._head._next
+        self._head = self._head.next
         return e
+
+    def print_all(self):
+        p = self._head
+        while p is not None:
+            print(p.elem, end='')
+            if p.next is not None:
+                print(', ', end='')
+            p = p.next
+        print('')
+
+if __name__ == '__main__':
--- DSAndAlgorithmsInPython/chapter3/LNode.py
+++ DSAndAlgorithmsInPython/chapter3/LNode.py
@@ -1,4 +1,4 @@
 class LNode:
     def __init__(self, elem, next_=None):
         self.elem = elem
-        self.next_ = next_
+        self.next = next_
