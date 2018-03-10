f7da79baa1cff2cd0b82c6a84df2c5958e741211
DuYeHong <c.fans@foxmail.com>
2018-03-10 23:58:22 +0800

    need complate

--- LeetCode/StringTointeger.py
+++ LeetCode/StringTointeger.py
@@ -1,12 +1,38 @@
 import re
+import unittest as ut
+
+class TestSolutionMethods(ut.TestCase):
+    def test_myAtoi(self):
+        s = Solution()
+        s1 = '-123'
+        s2 = '0'
+        s3 = ''
+        s4 = '-'
+
+        self.assertEqual(-123, s.myAtoi(s1))
+        self.assertEqual(0, s.myAtoi(s2))
+        self.assertEqual(0, s.myAtoi(s3))
+        self.assertEqual(0, s.myAtoi(s4))
 
 class Solution:
     def myAtoi(self, str):
         """
+        possible input like : '+-1', '+--3', ...
         :type str: str
         :rtype: int
         """
-        intPattern = re.compile("^[0-9-]+$")
-        if re.match(intPattern, str) is False:
-            raise ValueError("Invalid input")
+        if str is '-':
+            return 0
 
+        intPattern = re.compile("^[0-9-]+$")
+        if re.match(intPattern, str) is None:
+            return 0
+
+        flag = None
+        for x in str:
+            value = x == ''
+            if
+        return int(str)
+
+if __name__ == '__main__':
+    ut.main()
